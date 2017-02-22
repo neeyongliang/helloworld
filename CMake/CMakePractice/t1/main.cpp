c4531d73bc5bb524fb395b9d1779e0f7856528c0
yongliang <neeyongliang@gmail.com>
2017-02-22 23:34:39 +0800

    CMakePractice

---
+++ CMake/CMakePractice/t1/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2017 create by wiki
+ *
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+  cout << "hello, world!" << endl;
+  return 0;
+}
+
---
+++ CMake/CMakePractice/t2/src/main.cpp
@@ -0,0 +1,16 @@
+// Copyright 2017.
+// All rights reserved.
+//
+// Author: https://github.com/wikinee
+//
+// This file is for test
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+  cout << "hello, world!" << endl;
+  return 0;
+}
+
