6506cb699deaed245a1f54b32b6afd6e13deed52
yongliang <neeyongliang@gmail.com>
2019-06-01 18:06:49 +0800

    complate test-smbc

--- Python/single-file/test-smbc.py
+++ Python/single-file/test-smbc.py
@@ -5,16 +5,19 @@ import smbc
 import os
 import stat
 from pprint import pprint
+import time
+import shutil
 
 WORKGROUP = "WORKGROUP"
-SERVER = "172.30.36.101"
-SHARE = "share_for_all"
-USERNAME = "cdos"
-PASSWORD = "heheda"
-DESTDIR = "tmp/MyTest"
+SERVER = "192.168.0.10"
+SHARE = "MyTest"
+USERNAME = "apple"
+PASSWORD = "123456"
+DESTDIR = "test"
 
 BASE_URI = "smb://" + SERVER + '/' + SHARE
 DEST_URI = BASE_URI + '/' + "ubuntu-18.04.1-desktop-amd64.iso"
+FILE_URI = '/home/apple/ubuntu-18.04.1-desktop-amd64.iso'
 
 # print(DEST_URI)
 
@@ -46,6 +49,35 @@ def test_write():
     # mode = st[stat.ST_MODE]
     # print(mode)
     print(os.path.getsize('/home/apple/MyTest/ubuntu-18.04.1-desktop-amd64.iso'))
+
+def get_time_stamp():
+    ct = time.time()
+    local_time = time.localtime(ct)
+    data_head = time.strftime("%Y-%m-%d %H:%M:%S", local_time)
+    data_secs = (ct - int(ct)) * 1000
+    time_stamp = "%s.%03d" % (data_head, data_secs)
+    return time_stamp
+
+def test_shutil():
+    print(get_time_stamp())
+    ctx = smbc.Context(auth_fn=my_auth_callback_fn)
+    with open(FILE_URI, 'rb') as source_file_obj:
+        dest_file_obj = ctx.open(DEST_URI, os.O_CREAT | os.O_WRONLY)
+        shutil.copyfileobj(source_file_obj, dest_file_obj, 3 * 1024 * 1024)
+    print(get_time_stamp())
+
+def test_write():
+    print(get_time_stamp())
+    ctx = smbc.Context(auth_fn=my_auth_callback_fn)
+    with open(FILE_URI, 'rb') as source_file_obj:
+        dest_file_obj = ctx.open(DEST_URI, os.O_CREAT | os.O_WRONLY)
+        while True:
+            slice_data = source_file_obj.read(3 * 1024 * 1024)
+            if not slice_data:
+                break
+            ret = dest_file_obj.write(slice_data)
+    print(get_time_stamp())
+
 
 if __name__ == '__main__':
     test_opendir(BASE_URI + '/' + DESTDIR)
