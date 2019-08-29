fcd927d5f14b13ede0f82b760d28238d31d30b12
yongliang <neeyongliang@gmail.com>
2019-08-29 19:34:10 +0800

    update test-unpack-deb-pkgs.py

--- Python/single-file/test-unpack-deb-pkgs.py
+++ Python/single-file/test-unpack-deb-pkgs.py
@@ -5,13 +5,31 @@ import os
 import subprocess
 import sys
 
+def get_deb_files(deb_path):
+    deb_files = []
+    for root, dirs, files in os.walk(deb_path, topdown=False):
+        for name in files:
+            # print(os.path.join(root, name))
+            deb_files.append(os.path.join(root, name))
+        # for name in dirs:
+        #     print(os.path.join(root, name))
+    return deb_files
+
 def unpack_deb(deb_path):
-    deb_files = os.listdir(deb_path)
+    deb_files = get_deb_files(deb_path)
     for deb_file in deb_files:
-        if deb_file == "unpack.py":
-            continue
-        if not os.path.isdir(deb_file):
-            subprocess.call(["dpkg-deb", "-R", deb_file, os.path.splitext(deb_file)[0]])
+        try:
+            if deb_file == __file__:
+                continue
+            # if len(deb_file) < 4:
+            #     continue
+            # if deb_file[-4:] != ".deb" or deb_file[-5:0] != ".udeb":
+            if deb_file[-4:] != ".deb":
+                continue
+            basename = os.path.basename(deb_file)
+            subprocess.call(["dpkg-deb", "-R", deb_file, os.path.splitext(basename)[0]])
+        except Exception as e:
+            print("Unpack {} error, because {}".format(deb_file, e))
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -19,3 +37,4 @@ if __name__ == '__main__':
     else:
         pkg_path = sys.argv[1]
     unpack_deb(pkg_path)
+
