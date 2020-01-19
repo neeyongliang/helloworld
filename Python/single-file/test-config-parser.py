f53c741d74869f91ded7e43c411d1112b7494a60
yongliang <neeyongliang@gmail.com>
2020-01-19 14:31:38 +0800

    update configparser ignore case usage

--- Python/single-file/test-config-parser.py
+++ Python/single-file/test-config-parser.py
@@ -2,10 +2,20 @@
 testAccountService library
 """
 #!/usr/bin/python3
+# ConfigParser in python2
+from configparser import ConfigParser, NoSectionError
 
-import configparser
+class CustomConfigParser(ConfigParser):
+    """ config parser for conf file """
 
-cf = configparser.ConfigParser()
+    def __init__(self, defaults=None):
+        ConfigParser.__init__(self, defaults=defaults)
+
+    def optionxform(self, option_str):
+        """ case sensitive  """
+        return option_str
+
+cf = CustomConfigParser()
 
 cf.read("/etc/lightdm/lightdm.conf")
 
@@ -13,7 +23,7 @@ try:
     name = cf.get("SeatDefaults", "autologin-user")
     if name is not None:
         print("lucky %s, autologin next time!" % name)
-except configparser.NoSectionError:
+except NoSectionError:
     print("No Section Error!")
 except Exception as error_reason:
     print("failed: %s" % error_reason)
